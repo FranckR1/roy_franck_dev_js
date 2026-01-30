c9a674adfe253a08911ba72066799bb0c3203cf2
Franck <Franck.rd1@outlook.fr>
2026-01-30 17:43:59 +0100

    feat: create dir constants,controllers,services,routes and fix : route for my data in server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 const PORT = 3000;
 
-const countries = require('./countries.json');
+const countries = require('./data/countries.json');
 
 app.get('/countries', (req, res) => {
     res.send(countries)
