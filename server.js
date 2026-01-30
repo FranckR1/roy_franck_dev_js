65ad000db42b28429e2e6c412a59b49cc159f24b
Franck <Franck.rd1@outlook.fr>
2026-01-30 15:44:16 +0100

    feat: create api route for countries

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const app = express();
 
 const PORT = 3000;
 
+const countries = require('./countries.json');
+
+app.get('/countries', (req, res) => {
+    res.send(countries)
+})
+
 app.listen(PORT, () =>{
     console.log(`listenning on port ${PORT}`);
 });
