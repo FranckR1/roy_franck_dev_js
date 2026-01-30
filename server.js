3bad24d524d12ab5cd0af8e9ff3081ef91fb0ddc
Franck <Franck.rd1@outlook.fr>
2026-01-30 16:12:52 +0100

    feat : add route api for

--- server.js
+++ server.js
@@ -10,6 +10,33 @@ app.get('/countries', (req, res) => {
     res.send(countries)
 })
 
+app.get('/countries/low', (req, res) => {
+    const filter = countries.map(countrie => ({
+        name: countrie.name.common,
+        cca2: countrie.cca2,
+        cca3: countrie.cca3,
+        currencies: countrie.currencies,
+        languages: countrie.languages,
+        flag: countrie.flag,
+        capital: countrie.capital,
+        population: countrie.population,
+        continent: countrie.continents,
+    }));
+
+    res.send(filter)
+})
+
+app.get('/countries/short', (req, res) => {
+    const filter = countries.map(countrie => ({
+        name: countrie.name.common,
+        cca2: countrie.cca2,
+        cca3: countrie.cca3,
+        flag: countrie.flag,
+    }));
+
+    res.send(filter)
+})
+
 app.listen(PORT, () =>{
     console.log(`listenning on port ${PORT}`);
 });
