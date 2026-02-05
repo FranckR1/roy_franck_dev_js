6fd0932e6caa29f71e4a21d1c3139555749467ec
Franck <franck.rd1@outlook.fr>
2026-02-05 16:17:22 +0100

    feat : add variable for my connexion in my database and add connect for my base

--- db/db.js
+++ db/db.js
@@ -1,9 +1,21 @@
 import mariadb from 'mariadb';
 
 const pool = mariadb.createPool({
-    host: 'localhost',
-    user: '',
-    password: '',
+    host: process.env.MARIADB_HOST,
+    user: process.env.MARIADB_USER,
+    password: process.env.MARIADB_ROOT_PASSWORD,
+    database: process.env.MARIADB_DATABASE,
 });
 
-pool.getConnection();
+pool.getConnection()
+    .then(conn => {
+        console.log('Connexion à MariaDB réussie !');
+        conn.release();
+    })
+    .catch(err => {
+        console.error('Erreur de connexion à MariaDB:', err.message);
+    });
+
+export const getPool = () => {
+    return pool;
+};
--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 import express from 'express';
+import 'dotenv/config.js';
+import './db/db.js';
 
 const app = express();
 
 const PORT = 3000;
 
 import countrieRoute from './routes/countriesRoutes.js';
+import {getPool} from "./db/db.js";
 
 app.use(express.json());
 
 app.use('/countries', countrieRoute);
+
+try {
+    const pool = getPool();
+    const rows = await pool.query('SELECT * FROM etudiant');
+    console.log(rows);
+} catch (error) {
+    console.error(error);
+}
 
 app.listen(PORT, () =>{
     console.log(`listenning on port ${PORT}`);
